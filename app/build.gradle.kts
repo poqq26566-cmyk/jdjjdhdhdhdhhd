e995d08b7f9d425b068bcc9a13159782b83cdda6
poqq26566-cmyk <poqq26566@gmail.com>
2026-07-08 19:00:10 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,9 +40,10 @@ android {
 
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+          excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        excludes += "/META-INF/versions/9/OSGI-INF/MANIFEST.MF"
     }
+  }
 }
 
 dependencies {
